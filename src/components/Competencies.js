ce4c0ef4f6075898787f561452d3296d3bffc906
Gianluca <gianlucazin1990@gmail.com>
2021-03-18 17:08:38 +0100

    competencies section created

--- src/components/Competencies.js
+++ src/components/Competencies.js
@@ -1,5 +1,26 @@
 const Competencies = () => {
-  return <h1>Competencies</h1>;
+  return (
+    <div className="competencies ">
+      <div className="container u-max-width-small">
+        <h2 className="heading-secondary">Competencies.</h2>
+        <p className="paragraph">
+          FRONTEND: HTML5, CSS3/SCSS, Responsive Web Design, Javascript, JSX,
+          React, JSON, Webpack, D3.js, GSAP.
+        </p>
+        <p className="paragraph">
+          BACKEND: NodeJS, Express, REST, MongoDB, Mongooge, Chai Testing.
+        </p>
+        <p className="paragraph">
+          SKILLS: Organization, Collaboration, Leadership, Design, Business,
+          Marketing.
+        </p>
+        <p className="paragraph">
+          OTHER: Illustrator, Photoshop, Adobe XD, InDesign, GitHub, Git,
+          Trello.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Competencies;
