650db495d7aac1a007bc8e54c4d3b409ea899f20
Gianluca <gianlucazin1990@gmail.com>
2021-03-22 18:00:07 +0100

    nav menu hover integrated

--- src/layout/Navigation.js
+++ src/layout/Navigation.js
@@ -1,11 +1,34 @@
 import React, { useState, useRef, useEffect } from 'react';
+import ReactDOM from 'react-dom';
 import { gsap, Back, Expo, Power4 } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import braket from './../img/braket.svg';
 
 gsap.registerPlugin(ScrollTrigger);
 
+// Config
+const links = [
+  {
+    label: 'Projects',
+    url: './#projects',
+  },
+  {
+    label: 'Competencies',
+    url: './#competencies',
+  },
+  {
+    label: 'About',
+    url: './#about',
+  },
+  {
+    label: 'Contact',
+    url: './#contact',
+  },
+];
+
 const Navigation = () => {
-  const [tl] = useState(
+  // States
+  const [openMenuAnim] = useState(
     gsap.timeline({
       defaults: { duration: 1.5, ease: Back.easeOut.config(2) },
       paused: true,
@@ -14,26 +37,117 @@ const Navigation = () => {
   );
   const [menuOpen, setMenuOpen] = useState(false);
 
-  let navigationBtn = useRef();
+  // Refs
   let nav = useRef();
-  let item1 = useRef();
-  let item2 = useRef();
-  let item3 = useRef();
-  let item4 = useRef();
 
+  // Items
+  let items = useRef([]);
+  items.current = links.map(
+    (ref, index) => (items.current[index] = React.createRef())
+  );
+
+  // Brakets
+  let braketsLeft = useRef([]);
+  braketsLeft.current = links.map(
+    (ref, index) => (braketsLeft.current[index] = React.createRef())
+  );
+
+  let braketsRight = useRef([]);
+  braketsRight.current = links.map(
+    (ref, index) => (braketsRight.current[index] = React.createRef())
+  );
+
+  // Open menu animation
   useEffect(() => {
-    const items = [item1.current, item2.current, item3.current, item4.current];
-    tl.to(nav.current, { x: '10rem', visibility: 'visible' }).to(
-      items,
-      { opacity: 1, x: '0', stagger: 0.1 },
-      '-=1.5'
-    );
-  }, [tl]);
+    openMenuAnim.to(nav.current, { x: '10rem', visibility: 'visible' });
+    links.map((link, index) => {
+      openMenuAnim.to(
+        items.current[index].current,
+        { opacity: 1, x: '0', delay: 0.1 },
+        '-=1.5'
+      );
+    });
+  }, [openMenuAnim]);
+
+  // Hover animation menu
+  useEffect(() => {
+    links.map((link, index) => {
+      const hoverLeft = gsap.timeline({
+        defaults: { duration: 1, ease: Back.easeOut.config(2) },
+        paused: true,
+      });
+      const hoverRight = gsap.timeline({
+        defaults: { duration: 1, ease: Back.easeOut.config(2) },
+        paused: true,
+      });
+
+      hoverLeft.to(braketsLeft.current[index].current, {
+        x: 0,
+        opacity: 1,
+        rotationY: '0',
+      });
+      hoverRight.to(braketsRight.current[index].current, {
+        x: 0,
+        opacity: 1,
+        rotationY: '0',
+      });
+
+      const hoverPlay = () => {
+        hoverLeft.play();
+        hoverRight.play();
+      };
+      const hoverReverse = () => {
+        hoverLeft.reverse();
+        hoverRight.reverse();
+      };
+
+      items.current[index].current.addEventListener('mouseenter', hoverPlay);
+      items.current[index].current.addEventListener('mouseleave', hoverReverse);
+    });
+
+    return () => {
+      links.map((link, index) => {
+        const hoverPlay = () => {};
+        const hoverReverse = () => {};
+
+        items.current[index].current.removeEventListener(
+          'mouseenter',
+          hoverPlay
+        );
+        items.current[index].current.removeEventListener(
+          'mouseleave',
+          hoverReverse
+        );
+      });
+    };
+  }, []);
 
   const openMenu = () => {
-    !menuOpen ? tl.play() : tl.reverse(0.7);
+    !menuOpen ? openMenuAnim.play() : openMenuAnim.reverse(0.7);
     setMenuOpen(!menuOpen);
   };
+
+  const renderedList = links.map((link, i) => {
+    return (
+      <li key={i} ref={items.current[i]} className="navigation__item">
+        <img
+          ref={braketsLeft.current[i]}
+          src={braket}
+          alt="braket"
+          className="navigation__braket"
+        />
+        <a href={link.url} className="navigation__link">
+          {link.label}
+        </a>
+        <img
+          src={braket}
+          ref={braketsRight.current[i]}
+          alt="braket"
+          className="navigation__braket navigation__braket--right"
+        />
+      </li>
+    );
+  });
 
   return (
     <div className="navigation">
@@ -86,28 +200,7 @@ const Navigation = () => {
       </div>
 
       <nav ref={nav} className={`navigation__nav ${menuOpen ? 'open' : ''}`}>
-        <ul className="navigation__list">
-          <li ref={item1} className="navigation__item">
-            <a href="./#projects" className="navigation__link">
-              Projects
-            </a>
-          </li>
-          <li ref={item2} className="navigation__item">
-            <a href="./#competencies" className="navigation__link">
-              Competencies
-            </a>
-          </li>
-          <li ref={item3} className="navigation__item">
-            <a href="./#about" className="navigation__link">
-              About
-            </a>
-          </li>
-          <li ref={item4} className="navigation__item">
-            <a href="./#contact" id="user-page" className="navigation__link">
-              Contact
-            </a>
-          </li>
-        </ul>
+        <ul className="navigation__list">{renderedList}</ul>
       </nav>
     </div>
   );
